37d635fef9e891f04534c0a1498e0c805f843062
prashantbasel <[prashantbasel2@gmail.com]>
2026-05-08 10:25:23 +0545

    Day 1 - variables and if else

---
+++ day1.py
@@ -0,0 +1,45 @@
+# name = 'Prashant'
+# age = 21
+# course = 'Master in data science'
+
+# print ('my name is', name)
+# print ('my age is ', age)
+# print ('i am studying', course, 'at whitcliff')
+
+
+# score = 95
+# passing_marks = 40
+# failed = False
+
+# print (score)
+# print (passing_marks)
+# print (failed)
+
+# # simple math 
+
+
+# total = score + passing_marks
+# print ("Total:", total)
+
+
+city = 'kathmandu'
+uni = 'Whitecliff'
+gpa = 1.5
+passing_gpa = 2.5
+
+
+visa_approved = False
+
+print('I am from', city, 'and I will study at', uni , 'My gpa is', gpa, 'and my visa is approved:', visa_approved ) 
+
+
+# if else and elif
+
+if gpa >= 3.7:
+    print("Distinction")
+elif gpa >= 3.0:
+    print("Pass")
+elif gpa >= 2.0:
+    print("Borderline")
+else:
+    print("Failed")
