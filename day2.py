4c5af756b4cf80cea20d08ec1ee25d75bde4540b
prashantbasel <[prashantbasel2@gmail.com]>
2026-05-09 11:50:08 +0545

    day2-loops and even odd

---
+++ day2.py
@@ -0,0 +1,24 @@
+#for loop 
+
+# number = 9 
+# for i in range(1 , 11):
+#     print(number, "*", i, "=", number*i)
+
+
+ # while loop 
+
+# count = 1 
+
+# while count <= 5:
+#     print(count)
+#     count = count + 1   
+
+
+# practice 
+
+
+for i in range (1,21):
+    if i % 2 == 0:
+        print(i, "is", "even")
+    else:
+        print(i, "is", "odd")
