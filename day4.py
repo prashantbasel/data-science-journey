11a11371f786df98182f051a9dfa439f138a9504
prashantbasel <65806403+prashantbasel@users.noreply.github.com>
2026-05-14 11:40:59 +0545

    day 4 - functions

---
+++ day4.py
@@ -0,0 +1,6 @@
+def greet(name):
+    print("Hello", name)
+    print("welcome to day 4")
+greet("Prashant")
+greet("sam")
+greet("ram")
--- practice.py
+++ practice.py
@@ -10,3 +10,37 @@
 
 # average = sum(marks)/len(marks)
 # print("Averange", average)
+
+
+# Day 4 practice
+
+
+# def add(a,b):
+#     result = a + b 
+#     return result
+# answer = add(10, 5)
+# print(answer)
+
+
+# print(add(100 , 200))
+# print(add(7, 3))
+
+
+
+def calculate_average(marks):
+    average = sum(marks) / len(marks)
+    return average
+
+def check_pass(mark):
+    if mark >= 50:
+        return "pass"
+    else:
+        return "Fail"
+
+my_marks = [45, 78, 92, 55, 88]
+
+for mark in my_marks:
+    print(mark, "-", check_pass(mark))
+
+
+print("Average:", calculate_average(my_marks))
